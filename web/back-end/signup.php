47cbdd96b02b89a7467271f06bab00225c6d9afe
LuisDEV <etecluisgust@gmail.com>
2025-05-04 18:12:57 -0300

    Back end 1
    Entrada de dados: usuarios, e-mails e senhas

--- web/back-end/signup.php
+++ web/back-end/signup.php
@@ -1,58 +1,64 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form id="loginf" method="post" action="">
-        <label for="">email:</label>
-        <input name="email" id="email" type="text">
-        <label for="">senha:</label>
-        <input name="senha" id="senha" type="password">
-    </form>
-
-    window
-
-</body>
-</html>
-
 <?php
 
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha= "";
-    $bd = "reparte";
+$servidor = "localhost";
+$usuarioser = "root";
+$senhaser= "";
+$bd = "reparte";
 
-    $conexao = new mysqli($serv, $usuario, $senha, $bd);
+$conexao = new mysqli($servidor, $usuarioser, $senhaser, $bd);
 
-    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['senha']['email']) && !empty($_POST['email']['senha'])){
-
-
-        $email = floatval($_POST["email"]);
-        $senha = floatval($_POST["senha"]);
-
-
-    } else {
-
-                
-    }
-
-    if($conexao->connect_error){
+    if($conexao->connect_error)
+    {
         die("Falha na conexão : " . $conexao->connect_error);
     }
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
 
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        $usuario = $_POST["usuario"];
+
+        //Validando a entrada de dados
+        if(empty($email) || empty($senha) || empty($usuario))
+        {
+            echo "Faltou dados! :D";
+        }  
+            //Validando o email
+        else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            echo "Email inválido";
+        }
+        else
+        {
+
+            $stmt = $conexao->prepare("SELECT email FROM login WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($email_bd);
+            $stmt->fetch();
+
+            if($email_bd == $email)
+            {
+
+                echo "Email Já existe";
+                
+            } 
+            else 
+            {
+                $senha_has = password_hash($senha, PASSWORD_DEFAULT);
+                $stmt = $conexao->prepare("INSERT INTO `login`(`usuario`, `email`, `senha`) VALUES (?, ?, ?)");
+                $stmt->bind_param("sss", $usuario, $email, $senha_has);
+                
+                if ($stmt->execute()) {
+                    echo "Usuário cadastrado com sucesso!";
+                } else {
+                    echo "Erro ao cadastrar usuário: " . $stmt->error;
+                }
+
+            }
+
+        } 
+    }      
 
 ?>
-
-<script type="text/javascript">
-
-function erro() {
-    window.prompt("Faltou dados! :D")
-}
-
-
-
-</script>
---
+++ web/html/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form id="loginf" method="post" action="back-end/signup.php">
+        <label>usuario:</label>
+        <input name="usuario" id="usuario" type="text">
+        <label for="">email:</label>
+        <input name="email" id="email" type="text">
+        <label for="">senha:</label>
+        <input name="senha" id="senha" type="password">
+        <button type="submit">Enviar</button>
+    </form>
+
+</body>
+</html>
+
+<?php include '../back-end/signup.php'; ?>
